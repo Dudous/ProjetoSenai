463342f13669449b2354ed4635fde18c817a36dd
Dudous <sampaioeeduardo36@gmail.com>
2024-03-04 17:19:40 -0300

    pra apresentar

--- Tabela/script.js
+++ Tabela/script.js
@@ -46,3 +46,31 @@ function exportarParaExcel(){
     var wb = XLSX.utils.table_to_book(tabela, {sheet: "Estacionamento"});
     XLSX.writeFile(wb, nomeArquivo);
 }
+
+function removerItem(){
+    var nomeParaRemover = document.getElementById("nomeRemover").value;
+  if (!nomeParaRemover) {
+    alert("Digite um nome para remover.");
+    return;
+  }
+
+  var tabela = document
+    .getElementById("tabela")
+    .getElementsByTagName("tbody")[0];
+  var linhas = tabela.getElementsByTagName("tr");
+
+  // percorrer sobre todas as linhas da tabela
+  for (var i = 0; i < linhas.length; i++) {
+    // Obtendo a primeira célula (td) da linha atual
+    var celulaNome = linhas[i].getElementsByTagName("td")[0];
+
+    // Verificando se a célulaNome existe e se o conteúdo é igual ao nome a ser removido
+    if (celulaNome && celulaNome.innerHTML === nomeParaRemover) {
+      // Removendo a linha da tabela
+      tabela.deleteRow(i);
+
+      return;
+    }
+    }
+    document.getElementById("nomeRemover").value = "";
+}
